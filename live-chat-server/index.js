a20e5fdd4c88703233471e1fa29deb4974fa4ff5
ShubhamGupta9453 <shubhamgupta9454666551@gmail.com>
2024-07-22 22:24:18 +0530

    deployement

--- live-chat-server/index.js
+++ live-chat-server/index.js
@@ -40,5 +40,5 @@ app.use("/message", messageRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, console.log("Server is Running..."));
